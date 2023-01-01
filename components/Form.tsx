0bc9b75c3358ade5c20425968d419202b94acfb1
Joe Previte <jjprevite@gmail.com>
2023-01-01 00:57:49 +0000

    almost there

--- components/Form.tsx
+++ components/Form.tsx
@@ -28,7 +28,7 @@ const LoadingBtn = () => {
   return (
     <button
       type="button"
-      className="inline-flex items-center px-6 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed"
+      className="btn btn-primary normal-case ml-2 transition ease-in-out duration-150 cursor-not-allowed"
       disabled
     >
       <svg
--- components/ImageResult.tsx
+++ components/ImageResult.tsx
@@ -8,22 +8,22 @@ type ImageResultProps = {
 }
  
 
+// https://3000--dev--egghead--jsjoeio--apps.dev.coder.com/_next/image?url=https%3A%2F%2Freplicate.delivery%2Fpbxt%2FIAI6LT6jmNrcGJ2lF3EgmLObspTMpGUIT4mZ2WQVFTvnL6DE%2Fout-0.png&w=2048&q=75
 export function ImageResult({ prediction }: ImageResultProps) {
   return (
     <>
       {prediction && (
         <div>
           {prediction.output && (
-            <div>
+            //     position: relative;
+            <div className="relative w-80 h-auto mx-auto mt-8">
               <Image
                 fill
                 src={prediction.output[prediction.output.length - 1]}
                 alt="output"
-                sizes="100vw"
               />
             </div>
           )}
-          <p className="mt-4">status: {prediction.status}</p>
         </div>
       )}
     </>
